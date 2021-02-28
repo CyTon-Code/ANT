f964a5d4cf8e0543a839c20e44251db1e385157d
CyTon Code <cytoncode@gmail.com>
2021-03-01 00:57:27 +0200

    Update ANT если файлы пусты - FALSE если файл не пуст - TRUE

--- main.py
+++ main.py
@@ -1,43 +1,25 @@
 from additional.run_arguments import run_arguments
 from additional.run_scripts import run_scripts
 from additional.run_console import run_console
+from additional.ANT import ANT
 import sys
-
-
-class ANT:
-    def __init__(self):
-        self.home = __file__  # Папка расположения.
-
-        self.mode_console = True  # Можно запускать интерактивную консоль?
-        self.mode_arguments = True  # Можно читать аргументы как команду? как правило в ней запускают модуль arg ()
-        self.mode_scripts = False  # Можно запускать скриптовые файлы типов .a .py?
-
-    def set_console(self, console):
-        self.mode_console = console
-
-    def set_arguments(self, arguments):
-        self.mode_arguments = arguments
-
-    def set_scripts(self, scripts):
-        self.mode_scripts = scripts
-
-    def set_home(self, home):
-        self.home = home
-
+from additional.copy_file import copy_file
 
 ant = ANT()
-# TODO: Открывать файлы и
-#     изменять mode_arguments,
-#     mode_scripts, mode_console
-#     bool(copy_file("scripts.txt"))
-#     bool(copy_file("arguments.txt"))
-#     bool(copy_file("console.txt"))
 
-if ant.mode_arguments:
+# TODO: перейти позже на SQL.
+ant.mode_scripts = bool(copy_file("bd_v1/scripts.txt"))
+ant.mode_console = bool(copy_file("bd_v1/console.txt"))
+ant.mode_arguments = bool(copy_file("bd_v1/arguments.txt"))
+
+if ant.mode_arguments:  # использовать аргументы при запуске ANT:
+    print("start: arguments")
     run_arguments(sys.argv)
 
-if ant.mode_scripts:
-    run_scripts()
+if ant.mode_scripts:  # использовать скрипты при запуске ANT:
+    print("start: scripts")
+    run_scripts(ant.ant_path)
 
-if ant.mode_console:
+if ant.mode_console:  # использовать общение пользователя с ANT:
+    print("start: console")
     run_console()
